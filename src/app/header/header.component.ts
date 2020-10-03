8cfbb8e7f319b68fabc68ea3a486e59b08031273
hanafa01 <hana_fakhouri@hotmail.com>
2020-10-04 00:50:07 +0300

    toggle manage button and hamburger

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   navbarCollapsed: Boolean = true;
+  dropdown: Boolean = false;
 
   constructor() { }
 
@@ -16,6 +17,10 @@ export class HeaderComponent implements OnInit {
 
   onToggleNavbarCollapsed(): void {
     this.navbarCollapsed = !this.navbarCollapsed;
+  }
+
+  dropdownclick(){
+    this.dropdown = !this.dropdown;
   }
 
 }
