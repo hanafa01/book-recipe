c2fd74f8aa1a87f74101ee3fde2a133a4efb559b
hanafa01 <hana_fakhouri@hotmail.com>
2020-10-04 23:35:11 +0300

    styling active recipe items

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -18,7 +18,7 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
-        this.id = params['id'];
+        this.id = +params['id'];
         this.recipe = this.recipeService.getRecipe(this.id);
       }
     );
