89dc46c58d87fc41ce427af643bac21eb49f21a9
hanafa01 <hana_fakhouri@hotmail.com>
2020-10-04 21:18:12 +0300

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -7,7 +8,10 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
 
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe","This is simply a test","https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+    new Recipe("A Test Recipe","This is simply a test","https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg", [
+      new Ingredient('Meat',1),
+      new Ingredient('French Fries', 10)
+    ])
   ];
 
   constructor() { }
