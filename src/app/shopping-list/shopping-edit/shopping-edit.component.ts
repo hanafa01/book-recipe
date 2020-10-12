aa31d1548d0eb31f2b0b4a8167994e5125fb04a3
hanafa01 <hana_fakhouri@hotmail.com>
2020-10-13 01:00:09 +0300

    Allow to clear the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -46,6 +46,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     form.reset();
   }
 
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  
   ngOnDestroy(){
     this.subscription.unsubscribe();
   }
